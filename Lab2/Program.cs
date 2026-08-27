688b92e8ac36ee2b720ad446e997e8eaf59f6c07
alleyalle <168626275+alleyalle@users.noreply.github.com>
2026-08-28 00:22:22 +0700

    Added

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace Lab2
+﻿/*
+ * Student ID : 1690701469
+ * Name       : natchanun kosaiyaseth
+ * Section    : 129B
+ * No.        : 0
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace Lab2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+            Console.WriteLine($"Attack Power: {attackPower}");
+            Console.WriteLine($"Crit Multiplier: {critMultiplier}");
+            Console.WriteLine($"Is Boss: {isBoss}");
+            Console.WriteLine();
+
+            int hpPersent = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP Percent: {hpPersent}%");
+            Console.WriteLine();
+
+            Console.WriteLine($"Kirin takes 60 damage!");
+            currentHp = currentHp - 60;
+            Console.WriteLine();
+
+            Console.WriteLine("===== BOSS STATUS: AFTER DAMAGE =====");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+            hpPersent = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP Percent: {hpPersent}%");
         }
     }
 }
