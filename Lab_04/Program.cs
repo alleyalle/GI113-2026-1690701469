0d709f63c999e9da7a6a144570baf1d1a1ec4b39
alleyalle <168626275+alleyalle@users.noreply.github.com>
2026-09-07 22:59:33 +0700

    AddedAll04

---
+++ Lab_04/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Student ID : 1690701469
+ * Name       : Natchanun kosaiyaseth
+ * Section    : 129B
+ * No.        : -
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace Lab_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string CardName = "Gadget Card";
+            var itemName = "Anesthetic Watch";
+            var itemType = "Support";
+            string itemAbility = "Fires a sleeping needle that knocks out the target on hit.";
+            char rarityTier = 'A';
+            int level = 153;
+            float itemEnergy = 350.75f;
+            double itemPower = 149.65;
+            bool isAwakened = true;
+            double levelDouble = level;
+            int powerTruncated = (int)itemPower;
+            int powerRounded = Convert.ToInt32(itemPower);
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($"I         {CardName}        I");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("I             @___                I");
+            Console.WriteLine("I               __I_              I");
+            Console.WriteLine("I           _--      --_          I");
+            Console.WriteLine("I          ' @    @    @'         I");
+            Console.WriteLine(@"I          \    @    @  /         I");
+            Console.WriteLine(@"I           \ @     @  /          I");
+            Console.WriteLine(@"I            \    @   /           I");
+            Console.WriteLine("I              ------             I");
+            Console.WriteLine($"I       {itemName} : {itemType,-14}I");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("I             Ability             I");
+            Console.WriteLine("I                                 I");
+            Console.WriteLine($"I  {itemAbility,-31}I");
+            Console.WriteLine("I                                 I");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("I                                 I");
+            Console.WriteLine($"I  LVL         : {level,-17}I");
+            Console.WriteLine($"I  Rarity      : {rarityTier,-17}I");
+            Console.WriteLine($"I  Power       : {itemPower,-17}I");
+            Console.WriteLine($"I  Energy      : {itemEnergy,-17}I");
+            Console.WriteLine($"I  Awaken      : {isAwakened,-17}I");
+            Console.WriteLine("I                                 I");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("I             Log                 I");
+            Console.WriteLine("I                                 I");
+            Console.WriteLine($"I  Lv as double  : {levelDouble,-15}I");
+            Console.WriteLine($"I  Power (cast)  : {powerTruncated,-15}I");
+            Console.WriteLine($"I  Power Convert : {powerRounded,-15}I");
+            Console.WriteLine("I                                 I");
+            Console.WriteLine("-----------------------------------");
+        }
+    }
+}
